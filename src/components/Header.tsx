5a583f20d0e88fdf4afd88d1cee338e12556ccb4
birobirobiro <birobirobiro.dev@gmail.com>
2021-04-28 23:31:03 -0300

    :rocket: change image and version

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,24 @@
-import React, { useEffect, useState } from 'react';
-import { StyleSheet, View, Text, Image } from 'react-native';
-import { getStatusBarHeight } from 'react-native-iphone-x-helper';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, { useEffect, useState } from "react";
+import { StyleSheet, View, Text, Image } from "react-native";
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-import userImg from '../assets/biro.png';
-import colors from '../styles/colors';
-import fonts from '../styles/fonts';
+import userImg from "../assets/plant.png";
+import colors from "../styles/colors";
+import fonts from "../styles/fonts";
 
 export function Header() {
   const [userName, setUserName] = useState<string>();
 
   useEffect(() => {
     async function loadStorageUserName() {
-      const user = await AsyncStorage.getItem('@plantmanager:user');
+      const user = await AsyncStorage.getItem("@plantmanager:user");
 
-      setUserName(user || '');
+      setUserName(user || "");
     }
 
-    loadStorageUserName()
-  }, [userName])
+    loadStorageUserName();
+  }, [userName]);
 
   return (
     <View style={styles.container}>
@@ -29,32 +29,32 @@ export function Header() {
 
       <Image source={userImg} style={styles.image} />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
-    width: '100%',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
     paddingVertical: 20,
     marginTop: getStatusBarHeight(),
   },
   image: {
     width: 70,
     height: 70,
-    borderRadius: 40
+    borderRadius: 40,
   },
   greeting: {
     fontSize: 32,
     color: colors.heading,
-    fontFamily: fonts.text
+    fontFamily: fonts.text,
   },
   username: {
     fontSize: 32,
     fontFamily: fonts.heading,
     color: colors.heading,
-    lineHeight: 40
+    lineHeight: 40,
   },
 });
